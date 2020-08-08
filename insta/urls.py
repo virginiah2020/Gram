bc129000849a5dfa75888af406d36c52122481e5
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-08 21:55:51 +0300

    imported redirect

--- insta/urls.py
+++ insta/urls.py
@@ -3,3 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.home,name='home'),
+
+]
--- insta/views.py
+++ insta/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 
 # Create your views here.
 def home(request):
     current_user = request.user
-    all_images = Image.objects.all()
-    comments = Comment.objects.all()
-    likes = Likes.objects.all
-    profile = Profile.objects.all()
-    print(likes)
-    return render(request,'home.html',locals())
+    # all_images = Image.objects.all()
+    # comments = Comment.objects.all()
+    # likes = Likes.objects.all
+    # profile = Profile.objects.all()
+    # print(likes)
+    # return render(request,'home.html',locals())
