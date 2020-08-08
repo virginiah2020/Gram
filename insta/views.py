b621a32d5531495b329fad3b12ea991d376e579a
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-08 21:41:14 +0300

    added def home

--- insta/views.py
+++ insta/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 # Create your views here.
+def home(request):
+    current_user = request.user
+    all_images = Image.objects.all()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
