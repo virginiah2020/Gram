c1824dfbfc38cdb8b464218fd0b335f021fbceb4
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-10 06:05:45 +0300

    registered models on admin

--- insta/models.py
+++ insta/models.py
@@ -32,10 +32,25 @@ class Profile(models.Model):
         return profiles
 
     class Image(models.Model):
-    pic=ImageField(manual_crop='1080x800', blank=True)
-    name= models.CharField(max_length=55)
-    caption = models.TextField(blank=True)
-    profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
-    profile_details = models.ForeignKey(Profile)
+        pic=ImageField(manual_crop='1080x800', blank=True)
+        name= models.CharField(max_length=55)
+        caption = models.TextField(blank=True)
+        profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
+        profile_details = models.ForeignKey(Profile)
+    
 
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
 
