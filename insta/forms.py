be0f77cd0c62fa99c0f9758f597d7f4d3f27af38
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-10 07:09:39 +0300

    added class commentForm

--- insta/forms.py
+++ insta/forms.py
@@ -5,3 +5,14 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude=['owner']
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude =['likes','profile']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['image', 'comment_owner']
