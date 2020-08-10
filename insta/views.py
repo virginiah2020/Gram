fb4acc4604966f597c4f30601202d0e73603d165
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-10 06:17:50 +0300

    added on home.html

--- insta/views.py
+++ insta/views.py
@@ -4,10 +4,22 @@ from .models import Image, Profile,Comment
 # Create your views here.
 def home(request):
     current_user = request.user
-    # all_images = Image.objects.all()
-    # comments = Comment.objects.all()
-    # likes = Likes.objects.all
-    # profile = Profile.objects.all()
-    # print(likes)
-    # return render(request,'home.html',locals())
-    return HttpResponse
+    all_images = Image.objects.all()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
+
+def add_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.profile = current_user
+            add.save()
+            return redirect('home')
+
+    else:
+        form = ImageForm()
