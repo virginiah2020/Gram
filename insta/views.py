31710664aeb2b462c23740a4cc71c89b22157563
VIRGINIAH WAMUYU <vancyvy254@gmail.com>
2020-08-10 06:31:10 +0300

    added atribute profile

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse,Http404
+from django.http  import Http404
 from .models import Image, Profile,Comment
 # Create your views here.
 def home(request):
@@ -23,3 +23,20 @@ def add_image(request):
 
     else:
         form = ImageForm()
+    
+    return render(request,'image.html',locals())
+
+def profile(request):
+    current_user = request.user
+    # profile_details = Profile.objects.get(owner_id=current_user.id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile =form.save(commit=False)
+            profile.owner = current_user
+            profile.save()
+    else:
+        form=ProfileForm()
+
+    return render(request, 'profile/new.html', locals())
+
